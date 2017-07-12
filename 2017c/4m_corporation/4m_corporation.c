9719ede7df6e8705173f97de81ac989584abaf08
Hsiangkai Wang <hsiangkai@gmail.com>
2017-07-12 23:32:07 +0800

    Solve 4M corporation.

--- 2017c/4m_corporation/4m_corporation.c
+++ 2017c/4m_corporation/4m_corporation.c
@@ -1,5 +1,57 @@
 #include <stdio.h>
 
+static int reduce_sum(int sum, int num, int min, int mean, int median)
+{
+  if (min + median >= 2 * mean)
+    return -1;
+
+  while (sum > num * mean) {
+    sum += min; 
+    sum += median;
+    num += 2;
+  }
+
+  return num;
+}
+
+static int increase_sum(int sum, int num, int max, int mean, int median)
+{
+  if (max + median <= 2 * mean)
+    return -1;
+
+  while (sum < num * mean) {
+    sum += max;
+    sum += median;
+    num += 2;
+  }
+
+  return num;
+}
+
+static int least_odd_department(int min, int max, int mean, int median)
+{
+  int sum = min + max + median;
+  int i = 3;
+
+  if (sum > i * mean) {
+    return reduce_sum (sum, i, min, mean, median);
+  } else {
+    return increase_sum (sum, i, max, mean, median);
+  }
+}
+
+static int least_even_department(int min, int max, int mean, int median)
+{
+  int sum = min + max + median + median;
+  int i = 4;
+
+  if (sum > i * mean) {
+    return reduce_sum (sum, i, min, mean, median);
+  } else {
+    return increase_sum (sum, i, max, mean, median);
+  }
+}
+
 static int process(int min, int max, int mean, int median)
 {
   if (min > max)
@@ -12,7 +64,39 @@ static int process(int min, int max, int mean, int median)
     return 1;
   }
 
-  return 0;
+  if (min > median)
+    return -1;
+
+  if (min > mean)
+    return -1;
+
+  if (max < median)
+    return -1;
+
+  if (max < mean)
+    return -1;
+
+  int sum;
+  sum = min + max;
+  if ((sum == (2 * mean)) && (sum == (2 * median))) {
+    return 2;
+  }
+
+  int least_odd = least_odd_department (min, max, mean, median);
+  int least_even = least_even_department (min, max, mean, median);
+
+  if (least_odd == -1)
+    return least_even;
+  if (least_even == -1)
+    return least_odd;
+
+  if ((least_odd == -1) && (least_even == -1))
+    return -1;
+
+  if (least_odd < least_even)
+    return least_odd;
+  else
+    return least_even;
 }
 
 int main()
